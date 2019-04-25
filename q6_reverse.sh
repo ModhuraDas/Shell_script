bf16a8016c5081edf8061a71d4a2426f0b68fbfd
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-04-26 01:17:03 +0530

    Add files via upload

---
+++ q6_reverse.sh
@@ -0,0 +1,25 @@
+#Q : Write script to print given number in reverse order, for eg. If no is 123 it must print as 321.
+
+#filename : q6_reverse.sh
+#command to run : bash q6_reverse.sh
+#Enter a Number: 12345
+
+echo "Enter a Number:"
+read a
+rev=0
+sd=0
+or=$a
+while [ $a -gt 0 ]
+do
+sd=`expr $a % 10`
+temp=`expr $rev \* 10`
+rev=`expr $temp + $sd`
+a=`expr $a / 10`
+done
+echo "Reverse of $or is $rev"
+
+: 'Output :
+Enter a Number:
+12345
+Reverse of 12345 is 54321
+'
