1372d86415c6ae15f6ea115e54bcdc442c83c159
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-04-26 01:13:15 +0530

    Create q3_loop.sh

---
+++ q3_loop.sh
@@ -0,0 +1,20 @@
+#Q : Write script to print nos as 5,4,3,2,1 using while loop.
+
+#filename : q3_loop.sh
+#command to run : bash q3_loop.sh
+
+#!/bin/sh
+a=5
+while [ $a -gt 0 ]
+do
+echo $a
+a=`expr $a - 1`
+done
+
+: 'Output : 
+5
+4
+3
+2
+1
+'
