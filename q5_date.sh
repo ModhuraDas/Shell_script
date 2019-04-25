5b6866d552584dcc12a02231d687fb15f5fd421b
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-04-26 01:15:32 +0530

    Create q5_date.sh

---
+++ q5_date.sh
@@ -0,0 +1,20 @@
+#Q : Write Script to see current date, time, username, and current directory.
+
+#filename : q5_date.sh
+#command to run : bash q5_date.sh
+
+echo "Date and Time"
+date
+echo "Username"
+whoami
+echo "Current directory"
+pwd
+
+: 'Output : 
+Date and Time
+Fri Apr 26 00:45:04 IST 2019
+Username
+user
+Current directory
+/home/user/prog_lang/OS
+'
