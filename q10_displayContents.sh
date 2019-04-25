c3b3cce94e452add40beae32ee8b0a5b46e0214f
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-04-26 01:01:08 +0530

    Create q10_displayContents.sh

---
+++ q10_displayContents.sh
@@ -0,0 +1,13 @@
+#Q : Write script to print contains of file from given line number to next given number of lines. For e.g. If we called this script as Q13 and run as : - $ Q13 5 5 myf , Here print contains of 'myf' file from line number 5 to next 5 line of that file.
+
+#filename : q10_displayContents.sh
+#command to run : bash q10_displayContents.sh 6 2 q8_rsum.sh
+
+
+if [ $# -ne 3 ]
+then
+echo -e " please provide correct number of arguments"
+else
+echo " Here print contains of 'myf' file from line number $1 to next $2 line of that file"
+cat -n $3 | sed -n $1,$2p
+fi
