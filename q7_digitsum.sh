2c2be25141ab5ee4656472f4565e83a22f20bc53
Modhura <32320483+modhura3797@users.noreply.github.com>
2019-04-26 01:17:26 +0530

    Add files via upload

---
+++ q7_digitsum.sh
@@ -0,0 +1,25 @@
+#Q : Write script to print given numbers sum of all digit, For eg. If no is 123 it's sum of all digit will be 1+2+3 = 6.
+
+#filename : q7_digitsum.sh
+#command to run : bash q7_digitsum.sh
+#Enter a Number: 12345
+
+
+echo "Enter a Number"
+read a
+sum=0
+sd=0
+or=$a
+while [ $a -gt 0 ]
+do
+sd=`expr $a % 10`
+sum=`expr $sum + $sd`
+a=`expr $a / 10`
+done
+echo "Sum digit of $or is $sum"
+
+: 'Output :
+Enter a Number
+12345
+Sum digit of 12345 is 15
+'
